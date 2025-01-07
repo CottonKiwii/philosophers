649bc8a16092723d32e96dc7273d5eeb06a2cda5
Julia Wolfram <julia.wolfram@gmx.at>
2025-01-07 15:25:42 +0100

    feat(main): creates philosophers

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,9 +6,17 @@
 /*   By: jwolfram <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 12:18:17 by jwolfram          #+#    #+#             */
-/*   Updated: 2025/01/07 12:18:59 by jwolfram         ###   ########.fr       */
+/*   Updated: 2025/01/07 15:21:19 by jwolfram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <pthread.h>
-#include <stdio.h>
+#ifndef	PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+int	ft_atoi(char *str);
+
+#endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,13 +6,35 @@
 /*   By: jwolfram <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 12:19:08 by jwolfram          #+#    #+#             */
-/*   Updated: 2025/01/07 12:30:19 by jwolfram         ###   ########.fr       */
+/*   Updated: 2025/01/07 15:24:41 by jwolfram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	main()
+void	*routine_init(void	*arg)
 {
-	printf("hello world\n");
+	(void)arg;
+	printf("Philosopher was created\n");
+	return (NULL);
+}
+
+int	main(int argc, char **argv)
+{
+	int			i;
+	int			status;
+	pthread_t	*thread;
+
+	if (argc < 5)
+		return (1);
+	i = 0;
+	thread = (pthread_t *)malloc((ft_atoi(argv[1]) + 1) * sizeof(pthread_t));
+	if (!thread)
+		return (1);
+	while (i < ft_atoi(argv[1]))
+	{
+		status = pthread_create(&(thread[i]), NULL, &routine_init, NULL);
+		pthread_join(thread[i], NULL);
+		i++;
+	}
 }
